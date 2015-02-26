41dac04116b86db3994c34b9661354b219bd3d99
Timothy Clifford <timothy.clifford@yahoo.com>
2015-02-26 13:29:57 +1100

    adding carsousel

--- app/app.js
+++ app/app.js
@@ -3,15 +3,14 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngTouch',
+  'angular-carousel.shifty',
+  'angular-carousel',
   'myApp.skills',
   'myApp.subjects',
-  'myApp.version'
+  'myApp.careers',
+  'myApp.version',
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/skills'});
 }]);
-
-angular.module('angular-carousel', [
-    'ngTouch',
-    'angular-carousel.shifty'
-]);
