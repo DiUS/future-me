1590c99c5e807c42d5fe8d7c3b1bce639bad31ac
Timothy Clifford <timothy.clifford@yahoo.com>
2015-02-26 12:40:41 +1100

    skills routes

--- app/app.js
+++ app/app.js
@@ -3,12 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'myApp.skills',
+  'myApp.subjects',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/skills'});
 }]);
 
 angular.module('angular-carousel', [
