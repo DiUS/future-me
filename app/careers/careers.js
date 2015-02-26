c48d52e6f1521f29a82bd96697fcd34898edf558
Timothy Clifford <timothy.clifford@yahoo.com>
2015-02-27 10:49:44 +1100

    last commit from hack day

--- app/careers/careers.js
+++ app/careers/careers.js
@@ -9,6 +9,26 @@ angular.module('myApp.careers', ['ngRoute', 'myApp.services'])
   });
 }])
 
-.controller('CareersCtrl', ['$scope', 'CareersSvc', function($scope, CareersSvc) {
-	$scope.careers = CareersSvc.query();
+.controller('CareersCtrl', ['$rootScope', '$scope', '$location', 'CareersSvc', function($rootScope, $scope, $location, CareersSvc) {
+	
+	if(!$rootScope.subjectChoice) {
+		$location.path('/');
+	}
+
+	$scope.results = [];
+
+	CareersSvc.query().$promise.then( function(data) {
+
+
+		$scope.careers = data.filter(function(n) {
+			return n.subject.find($rootScope.subjectChoice);
+		});
+		$scope.hero = $scope.careers[0];
+	});
+
+	$scope.subjects = function() {
+		$rootScope.subjectChoice = '';
+		$location.path('/subjects');
+	};
+
 }]);
